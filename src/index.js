e7aba27ce27f2710a46cea4e8ddace44e97c5676
kichalomex <kichalo23con@hotmail.com>
2023-03-28 18:37:27 -0600

    Paso 1
    Paso 1 paso 3.1

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+let data = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelance",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }    
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Test</h1>')
+})
+
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(data)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
